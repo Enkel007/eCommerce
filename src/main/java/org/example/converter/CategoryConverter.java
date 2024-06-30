1992f1fb3f60901102db5a068ca9a0726e843558
Enkel007 <eshehdula22@epoka.edu.al>
2024-06-30 09:58:28 +0200

    added hibernate.cfg.xml

--- src/main/java/org/example/converter/CategoryConverter.java
+++ src/main/java/org/example/converter/CategoryConverter.java
@@ -1,9 +1,7 @@
 package org.example.converter;
 
 import org.example.Entity.Category;
-import org.example.Entity.Product;
 import org.example.dto.CategoryDTO;
-import org.example.dto.ProductDTO;
 
 public class CategoryConverter {
     public static Category convertToEntity(CategoryDTO categoryDTO) {
